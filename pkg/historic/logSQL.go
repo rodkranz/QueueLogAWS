844bbc1e52c4068e8869105b3cc6665634e98aff
Rodrigo Lopes <rodrigo.lopes@olx.com>
2017-11-22 11:40:35 +0000

    improve name of variable

--- pkg/historic/logSQL.go
+++ pkg/historic/logSQL.go
@@ -5,14 +5,14 @@ import (
 	"os"
 	"path"
 	"time"
-
+	
 	"github.com/go-clog/clog"
 	"github.com/go-xorm/core"
 	"github.com/go-xorm/xorm"
-
+	
 	// Import SQLLite3
 	_ "github.com/mattn/go-sqlite3"
-
+	
 	"github.com/rodkranz/monitor/pkg/historic/migrations"
 	"github.com/rodkranz/monitor/pkg/message"
 )
@@ -30,75 +30,64 @@ func SQLWriter(cfg SQLConfig) (w Writer, err error) {
 		return w, fmt.Errorf("fail to create directories: %v", err)
 	}
 	connStr := "file:" + cfg.Path + "?cache=shared&mode=rwc"
-
 	x, err := xorm.NewEngine("sqlite3", connStr)
 	if err != nil {
 		clog.Warn("fail to connect to database: %v", err)
 		return w, fmt.Errorf("fail to connect to database: %v", err)
 	}
-
 	x.SetMapper(core.GonicMapper{})
 	x.SetLogger(xorm.NewSimpleLogger3(os.Stdout, xorm.DEFAULT_LOG_PREFIX, xorm.DEFAULT_LOG_FLAG, core.LOG_WARNING))
-
 	if err = migrations.Migrate(x); err != nil {
 		clog.Warn("migrate: %v", err)
 		return w, fmt.Errorf("migrate: %v", err)
 	}
-
-	if err = x.StoreEngine("InnoDB").Sync2(new(msg)); err != nil {
+	if err = x.StoreEngine("InnoDB").Sync2(new(record)); err != nil {
 		clog.Warn("sync database struct error: %v\n", err)
 		return w, fmt.Errorf("sync database struct error: %v", err)
 	}
-
 	x.ShowSQL(true)
-
 	return func(msg *message.Message) error {
-		has, err := x.Where("message_id = ?", *msg.MessageId).Get(new(msg))
+		has, err := x.Where("message_id = ?", *msg.MessageId).Get(new(record))
 		if err != nil {
 			return err
 		}
-
 		if has {
 			clog.Info("[messageID:%s] already imported.", *msg.MessageId)
 			clog.Warn("[messageID:%s] message ignored.", *msg.MessageId)
 			return nil
 		}
-
-		obj := &msg{
+		obj := &record{
 			MessageID:   *msg.MessageId,
 			Body:        *msg.Body,
 			Topic:       msg.Topic(),
 			CreatedUnix: time.Now().Unix(),
 		}
-
 		if _, err := x.Insert(obj); err != nil {
 			clog.Warn("Error insert %#v: %s", obj, err)
 			return err
 		}
-
 		clog.Warn("[messageID:%s] message registered [id:%d].", *msg.MessageId, obj.ID)
 		return nil
 	}, nil
 }
 
-// msg is model of information that will persist at database.
-type msg struct {
-	ID        int64  `xorm:"pk autoincr"`
-	MessageID string `xorm:"VARCHAR(255)"`
-	Topic     string `xorm:"VARCHAR(255)"`
-	Body      string `xorm:"Text"`
-
+// record is model of information that will persist at database.
+type record struct {
+	ID          int64     `xorm:"pk autoincr"`
+	MessageID   string    `xorm:"VARCHAR(255)"`
+	Topic       string    `xorm:"VARCHAR(255)"`
+	Body        string    `xorm:"Text"`
 	Created     time.Time `xorm:"-"`
 	CreatedUnix int64
 }
 
 // BeforeInsert before insert any record define the current time
-func (m *msg) BeforeInsert() {
+func (m *record) BeforeInsert() {
 	m.CreatedUnix = time.Now().Unix()
 }
 
 // AfterSet after set columns convert unix data to time.Time.
-func (m *msg) AfterSet(colName string, _ xorm.Cell) {
+func (m *record) AfterSet(colName string, _ xorm.Cell) {
 	if colName == "created_unix" {
 		m.Created = time.Unix(m.CreatedUnix, 0).Local()
 	}
